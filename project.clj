fd944099b9a35f9f4bda76c503778b20862a7a80
Andy Fingerhut <andy_fingerhut@alum.wustl.edu>
2018-07-30 11:51:15 -0700

    Add funjible-test-project.perf-test namespace
    Copied, then modified somewhat, from funjible project.  This version
    is intended to be run from the Leiningen command line, without using
    clojure.test, so it is more convenient to define my own command line
    arguments and options.

--- project.clj
+++ project.clj
@@ -3,6 +3,14 @@
   :url "http://github.com/jafingerhut/funjible-test-project"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.9.0"]]
+  :dependencies [[org.clojure/clojure "1.9.0"]
+                 [funjible "0.2.0-SNAPSHOT"]
+                 [org.clojure/data.avl "0.0.17"]
+                 [org.clojure/data.int-map "0.2.4"]
+                 ;;[org.clojure/data.priority-map "0.0.10"]
+                 ;;[org.flatland/useful "0.11.5"]
+                 ;;[org.clojure/tools.trace "0.7.6"]
+                 [criterium "0.4.2"]]
   ;;:profiles {:dev {:dependencies [[funjible "0.2.0"]]}}
-  :jvm-opts ^:replace ["-server" "-Xmx1024m"])
+  :jvm-opts ^:replace ["-server" "-Xmx1024m"]
+  :main funjible-test-project.perf-test)
